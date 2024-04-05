dcba5bed71ad52ba16298d5ca9eeee9d396018da
Taylor Nairn <taylornairn1@gmail.com>
2024-04-04 22:08:06 -0600

    Added client side password checking on the registration page.

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+
+// Database insertion code here
+
+// After successful registration, send user back to home page
+header('Location: index.html');
+exit; // Ensure script execution ends here
+?>
