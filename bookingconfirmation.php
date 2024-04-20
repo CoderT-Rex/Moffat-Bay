421aae4d497b395f56d4ae4edd0e15b959e887ba
Joshua T. Rex <128547463+CoderT-Rex@users.noreply.github.com>
2024-04-20 01:43:49 -0400

    Small Formatting Changes

--- bookingconfirmation.php
+++ bookingconfirmation.php
@@ -16,17 +16,17 @@
 <link rel="stylesheet" href="styles.css">
 <title>Reservation Summary - Moffat Bay Lodge</title>
 <?php
-	$host = "localhost"; // Database host
-	$dbname = "MoffatBay"; // Database name
-	$user = "LodgeAdmin"; // Database username
-	$pass = "password"; // Database password
-	
-	$conn = new mysqli($host, $user, $pass, $dbname);
-	
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+$host = "localhost"; // Database host
+$dbname = "MoffatBay"; // Database name
+$user = "LodgeAdmin"; // Database username
+$pass = "password"; // Database password
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
 </head>
 <body>
@@ -45,7 +45,7 @@
 					<li><a href="book.php">Reservations</a></li>
                     <?php
                     session_start(); // Start the session
-					// Check if the user is logged in
+                                     // Check if the user is logged in
                     if (isset($_SESSION['user_id'])) {
                         // If logged in, display profile and logout links
                         echo '<li><a href="profile.php">' . $_SESSION['user_id'] . '</a></li>';
@@ -56,80 +56,79 @@
                     }
                     ?>
                 </ul>
-            </div>
-        </div>
-    </div>
-    <div class="restricted-container bg-light">
-        <div class="row spacer">
-            <div class="col-12 px-5 text-center">
-                <h1 class="color-primary underline-secondary">Reservation Details</h1>
-            </div>
-        </div>
-        <div class="row pt-3 width-80">
-            <div class="col-12">
-            	<!-- Code that pulls information from database -->
+			</div>
+		</div>
+	</div>
+	<div class="restricted-container bg-light">
+		<div class="row spacer">
+			<div class="col-12 px-5 text-center">
+				<h1 class="color-primary underline-secondary">Reservation Details</h1>
+			</div>
+		</div>
+		<div class="row pt-3 width-80">
+			<div class="col-12">
+				<!-- Code that pulls information from database -->
             	<?php
-				$reservationID = $_SESSION["reservationID"];
-				$stmt0 = $conn->prepare("SELECT * FROM Reservation WHERE reservationID = ?");
-				$stmt0->bind_param('i', $reservationID);
-				if (!$stmt0->execute()) {
-					$_SESSION['errors'][] = "Error getting reservation details: " . $stmt0->error;
-				}
-				else {
-					// Get the results from the query
-    				$result = $stmt0->get_result();
+            $reservationID = $_SESSION["reservationID"];
+            $stmt0 = $conn->prepare("SELECT * FROM Reservation WHERE reservationID = ?");
+            $stmt0->bind_param('i', $reservationID);
+            if (! $stmt0->execute()) {
+                $_SESSION['errors'][] = "Error getting reservation details: " . $stmt0->error;
+            } else {
+                // Get the results from the query
+                $result = $stmt0->get_result();
 
-    				// Check for at least one row in the set
-    				if ($result->num_rows > 0) {
-        				// Fetch the data as an associative array
-        				$reservationDetails = $result->fetch_assoc();
+                // Check for at least one row in the set
+                if ($result->num_rows > 0) {
+                    // Fetch the data as an associative array
+                    $reservationDetails = $result->fetch_assoc();
 
-						// Set variables for all the data in the returned row
-						$name = $_SESSION["user_id"];
-						$email = $_SESSION["uniqueID"];
-						$room_size = $reservationDetails["room_type"];
-						$guests = $reservationDetails["number_of_guests"];
-						$checkin = $reservationDetails["check_in_date"];
-						$checkout = $reservationDetails["check_out_date"];
-						$costPerNight = $reservationDetails["cost_per_night"];
-						$totalCost = $reservationDetails["total_cost"];
+                    // Set variables for all the data in the returned row
+                    $name = $_SESSION["user_id"];
+                    $email = $_SESSION["uniqueID"];
+                    $room_size = $reservationDetails["room_type"];
+                    $guests = $reservationDetails["number_of_guests"];
+                    $checkin = $reservationDetails["check_in_date"];
+                    $checkout = $reservationDetails["check_out_date"];
+                    $costPerNight = $reservationDetails["cost_per_night"];
+                    $totalCost = $reservationDetails["total_cost"];
+                } else {
+                    $_SESSION['errors'][] = "No reservation found with ID: $reservationID";
+                }
+            }
 
-    				} else {
-    					$_SESSION['errors'][] = "No reservation found with ID: $reservationID";
-    				}
-				}
-            	
-				if (!empty($_SESSION['errors'])) {
-					header("Location: error.php"); // Redirect to error page or back to form page to display errors
-				}
-            	else {
-            	    ?>
-					<ul class="list-unstyled fs-24">
-						<li>Name: <?php echo $name; ?></li>
-						<li>Contact Email: <?php echo $email; ?></li>
-						<li>Room Size: <?php echo $room_size; ?></li>
-						<li>Number of Guests: <?php echo $guests; ?></li>
-						<li>Check-in Date: <?php echo $checkin; ?></li>
-						<li>Check-out Date: <?php echo $checkout; ?></li>
-						<li>Cost per Night: $<?php echo $costPerNight; ?></li>
-						<li>Total Cost: $<?php echo $totalCost; ?></li>
-					</ul>
-					<div class="d-flex justify-content-center">
-						<form action="cancelReservation.php" method="post" style="padding-right: 30px;">
-							<!-- Hidden input to store the reservationID -->
-    						<input type="hidden" name="reservationID" value="<?php echo $reservationID; ?>">
-							<button type="submit" class="btn btn-light">Cancel Reservation</button>
-						</form>
-						<form action="confirmReservation.php" method="post" class="me-3">
-							<button type="submit" class="btn btn-light">Confirm Reservation</button>
-						</form>
-                <?php 
-            	}
+            if (! empty($_SESSION['errors'])) {
+                header("Location: error.php"); // Redirect to error page or back to form page to display errors
+            } else {
                 ?>
+					<ul class="list-unstyled fs-24">
+					<li>Name: <?php echo $name; ?></li>
+					<li>Contact Email: <?php echo $email; ?></li>
+					<li>Room Size: <?php echo ucwords($room_size); ?></li>
+					<li>Number of Guests: <?php echo $guests; ?></li>
+					<li>Check-in Date: <?php echo $checkin; ?></li>
+					<li>Check-out Date: <?php echo $checkout; ?></li>
+					<li>Cost per Night: $<?php echo $costPerNight; ?></li>
+					<li>Total Cost: $<?php echo $totalCost; ?></li>
+				</ul>
+				<div class="d-flex justify-content-center">
+					<form action="cancelReservation.php" method="post"
+						style="padding-right: 30px;">
+						<!-- Hidden input to store the reservationID -->
+						<input type="hidden" name="reservationID"
+							value="<?php echo $reservationID; ?>">
+						<button type="submit" class="btn btn-light">Cancel Reservation</button>
+					</form>
+					<form action="confirmReservation.php" method="post" class="me-3">
+						<button type="submit" class="btn btn-light">Confirm Reservation</button>
+					</form>
+                <?php
+            }
+            ?>
                 </div>
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+	</div>
 
 	<footer class="text-light pt-4 pb-2">
 		<div class="row width-80">
